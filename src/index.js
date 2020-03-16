820d288e63a8498a4e5e6501e7fbca7304a9b602
Ephrem Demelash <demelashephrem@gmail.com>
2020-03-16 16:41:44 +0300

    add variables for read query

--- src/index.js
+++ src/index.js
@@ -1,44 +1,51 @@
-import * as React from "react";
-import { useMutation } from "@apollo/react-hooks";
-import { AppLoading } from "./loading";
+/* eslint-disable camelcase */
+import * as React from 'react'
+import { useMutation } from '@apollo/react-hooks'
+import { AppLoading } from './loading'
 
-export const useAddMutation = (add_query, read_query, m_method, m_name) => {
-  var a = m_method;
-  m_method = { m_method };
+export const useAddMutation = (
+  add_query,
+  read_query,
+  m_method,
+  m_name,
+  variables
+) => {
+  var a = m_method
+  m_method = { m_method }
   const [addItem, { loading, error, data }] = useMutation(add_query, {
     update: (cache, { data: m_method }) => {
-      const val = cache.readQuery({ query: read_query });
-      const fixtures = val[m_name];
-      const test ={}
+      const val = cache.readQuery({ query: read_query, variables: variables })
+      const fixtures = val[m_name]
+      const test = {}
       test[m_name] = fixtures.concat([m_method[a]])
-      console.log(m_method[a])
       cache.writeQuery({
         query: read_query,
+        variables: variables,
         data: test
-      });
+      })
     }
-  });
-  
-  return [addItem, { loading, error, data }];
-};
-export const useDeleteMutation = (add_query, read_query, m_method, m_name) => {
-  var a = m_method;
-  m_method = { m_method };
+  })
+
+  return [addItem, { loading, error, data }]
+}
+export const useDeleteMutation = (add_query, read_query, m_method, m_name, variables) => {
+  var a = m_method
+  m_method = { m_method }
   const [addItem, { loading, error, data }] = useMutation(add_query, {
     update: (cache, { data: m_method }) => {
-      const val = cache.readQuery({ query: read_query });
-      const fixtures = val[m_name];
-      const test ={}
-      test[m_name] = fixtures.filter(({id}) => id!==m_method[a].id)
-      console.log(m_method)
+      const val = cache.readQuery({ query: read_query, variables: variables })
+      const fixtures = val[m_name]
+      const test = {}
+      test[m_name] = fixtures.filter(({id}) => id !== m_method[a].id)
       cache.writeQuery({
         query: read_query,
+        variables: variables,
         data: test
-      });
+      })
     }
-  });
-  
-  return [addItem, { loading, error, data }];
-};
+  })
 
-export const Loading = AppLoading;
+  return [addItem, { loading, error, data }]
+}
+
+export const Loading = AppLoading
